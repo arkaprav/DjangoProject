a43adeb2b79dbc2d0249afdc952187f6d77b903b
arkaprrava-wisdmlabs <arkaprava.chakraborty@wisdmlabs.com>
2023-07-31 19:47:23 +0530

    completed the sopping page

--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
 from django.conf import settings
 from products.models import Category, Brand, Product
-from django.core.serializers.json import DjangoJSONEncoder
-
+from django.db.models import Min, Max
 import json
 import os
 def home(request):
@@ -30,23 +29,41 @@ def contact(request):
     return render(request,'contact.html',context=title)
 def shop(request):
     if request.method == 'POST':
-        data = request.POST.get('data',0)
-        if data != 0:
-            results = list(Product.objects.filter(price__range = ( 0, float(data) ) ).values())
-            for i in range(len(results)):
-                category = list(Category.objects.filter(id__exact = results[i]['category_id']).values_list()[0])
-                results[i]['category'] = category[1]
-                brand = list(Brand.objects.filter(id__exact = results[i]['brand_id']).values_list()[0])
-                results[i]['brand'] = brand[1]
-            return JsonResponse(list(results),safe=False, status=200)
+        price = request.POST.get('price',0)
+        brands = request.POST.getlist('brands[]')
+        categories = request.POST.getlist('categories[]')
+        results = list(Product.objects.all().values())
+        for i in range(len(results)):
+            category = list(Category.objects.filter(id__exact = results[i]['category_id']).values_list()[0])
+            results[i]['category'] = category[1]
+            brand = list(Brand.objects.filter(id__exact = results[i]['brand_id']).values_list()[0])
+            results[i]['brand'] = brand[1]
+        if price != 0:
+            if(categories != [] or brands != []):
+                answer = []
+                for i in range(len(results)):
+                    if results[i]['price'] <= float(price) and (results[i]['category'] in categories or results[i]['brand'] in brands):
+                        answer.append(results[i])
+                return JsonResponse(list(answer),safe=False, status=200)
+            else:
+                answer = []
+                for i in range(len(results)):
+                    if results[i]['price'] <= float(price):
+                        answer.append(results[i])
+                return JsonResponse(list(answer),safe=False, status=200)         
     c = Category.objects.all().exclude(num_products=0)
     p = Product.objects.all()
     b = Brand.objects.all().exclude(num_products=0)
+    min_value = Product.objects.aggregate(Min('price'))['price__min']
+    max_value = Product.objects.aggregate(Max('price'))['price__max']
+    print(min_value, max_value)
     title = {
         'title': 'Shop',
         'c': c,
         'p': p,
         'b': b,
+        'min': int(min_value),
+        'max': int(max_value),
         'media_link':settings.MEDIA_URL
     }
     return render(request,'shop.html',context=title)
--- static/js/nav.js
+++ static/js/nav.js
@@ -15,16 +15,86 @@ $(document).ready(function(){
     i.oninput = function () {
         o.innerHTML = i.value;
     };
-    i.onchange = function() {
+    categories = [];
+    $('.categories').on('click', function() {
+        if ($(this).is(':checked')) {
+            // Show an alert when the checkbox is checked
+            if(categories.indexOf($(this).attr('name'))==-1){
+                categories.push($(this).attr('name'));
+            }
+        }
+        else{
+            var index = categories.indexOf($(this).attr('name'));
+            if (index > -1) {
+                categories.splice(index, 1);
+            }
+        }
+      });
+      brands = [];
+    $('.brands').on('click', function() {
+        if ($(this).is(':checked')) {
+            // Show an alert when the checkbox is checked
+            if(brands.indexOf($(this).attr('name'))==-1){
+                brands.push($(this).attr('name'));
+            }
+        }
+        else{
+            var index = brands.indexOf($(this).attr('name'));
+            if (index > -1) {
+                brands.splice(index, 1);
+            }
+        }
+      });
+      var con = {
+        'brands':[],
+        'categories':[]
+      }
+      $('#i').on('click', function(e){
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: {
+                'price': i.value,
+                'brands': brands,
+                'categories':categories,
+                'csrfmiddlewaretoken': csrftoken,
+            },
+            success: function (response) {
+                con = {
+                    'brands':brands,
+                    'categories':categories
+                }
+                var insert = `<div class="items">`;
+                for(const key in response){
+                    let output = `
+                    <div style="background-image:linear-gradient(rgb(0,0,0,0.5),rgb(0,0,0,0.5)),url('${media_link}${response[key].featured_image}');">
+                        <h3 id="name">${response[key].name}</h3>
+                        <p class="price">price: ${response[key].price} INR </p>
+                        <p class="rating"><i class="fa fa-star" style="font-size: 18px;"></i> ${response[key].rating} out of 5</p>
+                        <p><span class="category">${response[key].category}</span>, <span class="brand">${response[key].brand}</span></p>
+                    </div>
+                    `;
+                    insert += output;
+                }
+                insert +=  '</div>';
+                document.getElementById('container').innerHTML = insert;
+            }
+        });
+      });
+      i.onchange = function() {
         var a = $(this).val();
         $.ajax({
             type: 'POST',
             url: url,
             data: {
-                'data': a,
+                'price': a,
+                'brands': con.brands,
+                'categories':con.categories,
                 'csrfmiddlewaretoken': csrftoken,
             },
             success: function (response) {
+                console.log(response);
                 var insert = `<div class="items">`;
                 for(const key in response){
                     let output = `
