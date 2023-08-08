262e1ea19210dc334f16608b49d14e724d7bbf2e
arkaprrava-wisdmlabs <arkaprava.chakraborty@wisdmlabs.com>
2023-08-08 21:46:56 +0530

    added empty logic to faourites page

--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
